245addd7296069a433ff5ee58a1b7c0608c304c2
Quezia Vieira <vieiraquezia24@gmail.com>
2025-06-18 13:54:41 -0300

    commit

--- src/home.jsx
+++ src/home.jsx
@@ -2,12 +2,15 @@ import "./App.css";
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import ListaPersonagens from './components/listaPersonagens.jsx';
+import BuscaHeroi from "./components/buscaHeroi.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-
+      <BuscaHeroi/> {/* Aqui fica a barra de busca */}
+      <h1>Personagens da Marvel</h1>
+      <p>Encontre seus heróis favoritos!</p>
       <ListaPersonagens />  {/* Aqui aparecem os cards da API com seu CSS */}
 
       <Footer />
